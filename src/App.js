6994c6bc9ef820e68aa57e6d758173d96f87228f
Stephen Catledge <sxc6227@psu.edu>
2023-12-14 23:22:34 -0500

    Added a placeholder gallery and background to website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import fugitive from './images/EnemyOfTheState.png';
 import './App.css';
+import { useState } from 'react';
+import {artGal} from './data.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Gallery() {
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+    const hasNext = index < artGal.length - 1;
+    const hasPrev = index > 0 || index === artGal.length;
+
+
+    function handlePrevClick() {
+        if (hasPrev) {
+            setIndex(index - 1);
+        } else {
+            setIndex(0);
+        }
+    }
+    function handleNextClick() {
+        if (hasNext) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
+    }
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
+    }
+
+    let sculpture = artGal[index];
+    return (
+        <>
+            <h1>Gallery of Sketches</h1>
+            <button onClick={handleNextClick}>
+                Next
+            </button>
+            <button onClick={handlePrevClick}>
+                Previous
+            </button>
+            <h2>
+                <i>{sculpture.name} </i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {artGal.length})
+            </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+        </>
+    );
 }
 
-export default App;
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={fugitive} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//export default App;
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const artGal = [{
+    name: 'Warlord',
+    url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+    alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
+}, {
+    name: 'Warlord Demon Form',
+    url: 'https://i.imgur.com/ZF6s192m.jpg',
+    alt: 'A gigantic metallic flower sculpture with reflective mirror-like petals and strong stamens.'
+}, {
+    name: 'Oni Mask',
+    url: 'https://i.imgur.com/aTtVpES.jpg',
+    alt: 'The sculpture depicting a human head seems ever-present and solemn. It radiates calm and serenity.'
+}, {
+    name: 'Higgs the Pirate',
+    url: 'https://i.imgur.com/RCwLEoQm.jpg',
+    alt: 'Three monumental stone busts with the heads that are disproportionately large with somber faces.'
+}, {
+    name: 'Wizard',
+    url: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    alt: 'A large mosaic sculpture of a whimsical dancing female figure in a colorful costume emanating joy.'
+}, {
+    name: 'Risk of Doom 3',
+    url: 'https://i.imgur.com/2heNQDcm.jpg',
+    alt: 'A tall sculpture made of three elements stacked on each other reminding of a human figure.'
+}, {
+    name: 'Outlaw',
+    url: 'https://i.imgur.com/wIdGuZwm.png',
+    alt: 'An intricate wood sculpture of a warrior with a focused face on a horse adorned with patterns.'
+}, {
+    name: 'Fungus Watcher',
+    url: 'https://i.imgur.com/AlHTAdDm.jpg',
+    alt: 'The sculpture reminds a cascade of folds, quite different from bellies in classical sculptures.'
+}, {
+    name: 'The Muse',
+    url: 'https://i.imgur.com/HMFmH6m.jpg',
+    alt: '12 terracotta sculptures of solemn warriors, each with a unique facial expression and armor.'
+}, {
+    name: 'The Beast',
+    url: 'https://i.imgur.com/rN7hY6om.jpg',
+    alt: 'A black matte sculpture where the individual elements are initially indistinguishable.'
+},];
